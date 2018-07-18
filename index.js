150edc6b59ec6a7f45b79a8545c79fb7b1f4f398
jarretbryan <jarret.bryan@gmail.com>
2018-07-18 17:30:09 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 //Your code here
+function justInvoke(fn) {
+  return fn();
+}
+
+function setThisWithCall(fn, thisValue, argument) {
+  return fn.call(thisValue, argument);
+}
+
+function setThisWithApply(fn, thisValue, args) {
+  return fn.apply(thisValue, args);
+}
+
+function returnNewFunctionOf(newFn, thisValue) {
+  const clone = newFn.bind(thisValue);
+  return clone;
+}
