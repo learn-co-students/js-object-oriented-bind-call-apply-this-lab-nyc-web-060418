ba4f51e908c97cde54ad62266c71a744421a26bf
Ms-Alex <ms.hernandeza1@gmail.com>
2018-07-19 14:04:34 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 //Your code here
+function justInvoke(fn){
+  return fn(this)
+}
+
+function setThisWithCall(fn, bob, num) {
+  return fn.call(bob, num)
+}
+
+function setThisWithApply(fn, bob, [age, hairColor]) {
+  return fn.apply(bob, [age, hairColor])
+}
+
+function returnNewFunctionOf(fnCopy, fred){
+  return fnCopy.bind(fred)
+}
